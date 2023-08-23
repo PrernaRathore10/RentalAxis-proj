5b20cfbfea183044c88ac5664876c95056d89aba
PrernaRathore10 <prerna25io043@satiengg.in>
2023-08-23 12:24:01 +0530

    login page color change to purple

--- RentalAxis/settings.py
+++ RentalAxis/settings.py
@@ -76,12 +76,25 @@ WSGI_APPLICATION = 'RentalAxis.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3',)
+#     }
+# }
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3',)
-    }
+	'default': {
+		'ENGINE': 'django.db.backends.mysql',
+		'NAME': 'rentalaxis',
+		'USER': 'root',
+		'PASSWORD': '1002',
+		'HOST':'localhost',
+		'PORT':'3306',
+	}
 }
+
 
 
 # Password validation
